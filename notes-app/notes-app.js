736c95c4a484b79e3248ca0ac917c4e64dbced97
Simon Minifie <simon@minifie.me>
2019-08-21 11:47:28 +0100

    course continues

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,11 +1,45 @@
-//Query and remove first item with tag
-// const p = document.querySelector('p')
-// p.remove()
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to the Canary Islands'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise'
+}, {
+    title: 'Office modifications',
+    body: 'Get a desk'
+}]
 
-const ps = document.querySelectorAll('p')
-console.log(ps)
-ps.forEach(function(p) {
-    // console.log(p.textContent)
-    // p.remove()
-    p.textContent = '******'
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase()) 
+    })
+
+    document.querySelector('#app').innerHTML = '<p>Test</p>'
+
+    filteredNotes.forEach(function(note) {
+        const noteElement = document.createElement('p')
+        noteElement.textContent = note.title
+        document.querySelector('#app').appendChild(noteElement)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function(e) {
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#remove-notes').addEventListener('click', function() {
+    document.querySelectorAll('.note').forEach(function(note) {
+        note.remove()
+    })
+})
+
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,44 @@
-const ps = document.querySelectorAll('p')
+const todos = [{
+    text: 'Cut the lawn',
+    completed: true
+}, {
+    text: 'Feed the cat',
+    completed: false
+}, {
+    text: 'Wash the car',
+    completed: true
+}, {
+    text: 'Learn javascript',
+    completed: false
+}, {
+    text: 'Cook tea',
+    completed: true
+}
+]
 
-ps.forEach(function(p) {
-    if (p.textContent.includes('the')) {
-        p.remove()
-    }
+//Calculate number of incomplete todos
+const incompleteTodos = todos.filter(function(todo) {
+    return !todo.completed
+})
+
+//Render number of incomplete todos
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+//List todos
+todos.forEach(function(todo) {
+        let p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('body').appendChild(p)
+})
+
+//Listen for new todo creation
+document.querySelector('#add-todo').addEventListener('click', function(e) {
+    console.log('I was clicked')
+})
+
+// Listen for new todo text change
+document.querySelector('#new-todo').addEventListener('input', function(e) {
+    console.log(e.target.value)
 })
