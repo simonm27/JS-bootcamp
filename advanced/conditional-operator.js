812bad711f325777bc8e0e1f2f52adf8134f6b6a
Simon Minifie <simon@minifie.me>
2019-08-29 16:42:04 +0100

    video 82

---
+++ advanced/conditional-operator.js
@@ -0,0 +1,22 @@
+// const myAge = 16
+// const message = myAge >=18 ? 'You can vote' : 'You cannot vote'
+
+// console.log(message)
+
+// const myAge = 12
+// const showPage = () => {
+//     return 'showing the page'
+// }
+
+// const showErrorPage = () => {
+//     return 'showing the error page'
+// }
+
+// const message = myAge >= 21 ? showPage() : showErrorPage()
+
+// console.log(message)
+
+const team = ['Tyler', 'Simon', 'Megan', 'Lauren']
+
+const message = team.length <= 4 ? `Team size: ${team.length}` : 'Too many people on the team'
+console.log(message)
