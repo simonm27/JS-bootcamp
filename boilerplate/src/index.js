6065c820813c6635cf0acce3128853bc0408b602
Simon Minifie <simon@minifie.me>
2020-01-02 14:46:21 +0000

    video 149

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,1 +1,22 @@
 
+// REST paramater starts with ...
+const calculateAverage = (thing, ...numbers) => {
+    //return (num + num2) / 2
+    let sum = 0
+    numbers.forEach((num) => sum += num)
+    const average =  sum / numbers.length
+    return `The average ${thing} is ${average}`
+}
+
+console.log(calculateAverage('age', 0, 100, 88, 64))
+
+
+const printTeam = (teamName, coach, ...players) => {
+    console.log(`Team: ${teamName}`)
+    console.log(`Coach: ${coach}`)
+    console.log(`Players: ${players.join(', ')}`)
+            
+}
+
+printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry')
+
